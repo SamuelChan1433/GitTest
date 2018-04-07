05895952b4821566adc2bf6bdc7764b93b3a5f4f
obrcnh <1173995314@qq.com>
2018-04-07 21:29:37 +0800

    add something

--- App.js
+++ App.js
@@ -12,13 +12,6 @@ import {
   View
 } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
 type Props = {};
 export default class App extends Component<Props> {
   render() {
@@ -26,10 +19,6 @@ export default class App extends Component<Props> {
       <View style={styles.container}>
         <Text style={styles.welcome}>
           Welcome to React Native!
-        </Text>
-
-        <Text style={styles.instructions}>
-          {instructions}
         </Text>
       </View>
     );
@@ -47,10 +36,5 @@ const styles = StyleSheet.create({
     fontSize: 20,
     textAlign: 'center',
     margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
   },
 });
